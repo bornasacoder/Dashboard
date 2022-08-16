7e6d19fc258393685b68810cc4bba65ffc24e1cd
Ashish <ashish8112633402@gmail.com>
2022-08-16 20:08:39 +0530

    welcome

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,7 +6,7 @@ export default function Topbar() {
     <div className='topbar'>
         <div className="topbarWrapper">
             <div className="topLeft">
-                <span className='logo'>AshishAdmin</span>
+                <span className='logo'>HarshAdmin</span>
             </div>
             <div className="topRight">
                 <div className="topbarIconContainer">
